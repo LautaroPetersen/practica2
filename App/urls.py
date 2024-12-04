ab2e29c083a2f1cf5c724ff306ac5b277adab8bd
LautaroPetersen <lautaropetersen@gmail.com>
2024-12-04 16:17:48 -0300

    se craron herencias de tamplate, models y otras correcciones

--- App/urls.py
+++ App/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from App import views 
 
 urlpatterns = [
-   path('inicio/',views.inicio)
+   path('',views.inicio, name="Inicio"),
+   path('about/',views.about, name="About"),
+   path('projects/',views.projects, name="Projects"),
+   path('signup/',views.signup, name="Signup")
 ]
+
--- App/views.py
+++ App/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, "App/inicio.html")
+def about(request):
+    return render(request, "App/about.html")
+def projects(request):
+    return render(request, "App/projects.html")
+def signup(request):
+    return render(request, "App/signup.html")
